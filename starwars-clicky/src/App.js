e89cb5e48dbbc7ba7a705254b88673022b8378c9
David Schwartz <davidschwartz23@gmail.com>
2019-08-06 21:16:45 -0700

    Making changes to App.js file

--- starwars-clicky/src/App.js
+++ starwars-clicky/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Jumbotron from "./components/jumbotron/Jumbotron";
+import Navbar from "./components/navbar/Navbar";
+import Card from "./components/cards/Card";
+import Footer from "./components/footer/Footer";
+import starwars from "./starwars.json";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//sets the state to zero
+class App extends Component {
+  state = {
+    characters,
+    clickedCharacters: [],
+    score: 0
+  }
 }
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
